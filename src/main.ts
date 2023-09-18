0f0a792afafcf8d9650979feabdbf889fd05d175
linbinghe <linbinghe@gmail.com>
2023-09-18 21:05:45 +0800

    feat: add https support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  let options = undefined;
+  if (process.env.httpsKey && process.env.httpsCert) {
+    options = {
+      key: readFileSync(process.env.httpsKey),
+      cert: readFileSync(process.env.httpsCert),
+    };
+  }
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions: options,
+  });
   await app.listen(7001);
 }
 bootstrap();
