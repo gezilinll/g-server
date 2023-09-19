1293e87c87673109c9e0d49ec442f138248c8177
linbinghe <linbinghe@gmail.com>
2023-09-19 08:18:00 +0800

    feat: remove https

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  let options = undefined;
-  if (process.env.httpsKey && process.env.httpsCert) {
-    options = {
-      key: readFileSync(process.env.httpsKey),
-      cert: readFileSync(process.env.httpsCert),
-    };
-  }
   const app = await NestFactory.create(AppModule, {
     cors: true,
-    httpsOptions: options,
   });
   await app.listen(7001);
 }
